32f62f2448359168e0cb15deb5bb80c757bccfa4
TAEUK-PARK <110377189+TAEUK-PARK@users.noreply.github.com>
2022-10-20 19:06:18 +0900

    feat: implement authenticate middleware
    * feat: implement authenticate middleware
    
    * refactor: change jwt.verify async at authenticate middleware
    
    * refactor: change jwt.verify async at auth.controller

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,1 +1,16 @@
-module.exports = () => {};
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
+
+const ERROR = require("../constants/error");
+
+module.exports = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.split(" ")[1];
+    await jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    next();
+  } catch (err) {
+    next(createError(401, ERROR.BAD_REQUEST));
+  }
+};
