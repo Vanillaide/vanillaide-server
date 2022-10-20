47bb0e12c6ac1c3e6fcb2c3707f818493d86432e
Sohyeon <needtlc13@gmail.com>
2022-10-20 17:34:21 +0900

    feat: implement getProjects and add time stamp in project schema

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,30 +5,59 @@ const User = require("../models/User");
 const Project = require("../models/Project");
 const ERROR = require("../constants/error");
 
-exports.getProjects = () => {};
+exports.getProjects = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+
+    if (!userId || !mongoose.isValidObjectId(userId)) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    const projects = await Project.aggregate([
+      {
+        $match: { creator: mongoose.Types.ObjectId(userId) },
+      },
+      { $sort: { updatedAt: -1 } },
+    ]);
+
+    res.status(200).json({ projects });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.postProject = async (req, res, next) => {
-  const { userId } = req.params;
-  const { projectName } = req.body;
+  try {
+    const { userId } = req.params;
+    const { projectName } = req.body;
 
-  if (!userId || !projectName || projectName.length > 50) {
-    return next(createError(400, ERROR.BAD_REQUEST));
+    if (!userId || !projectName || projectName.length > 50) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    if (!mongoose.isValidObjectId(userId)) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    await Project.create({
+      creator: userId,
+      name: projectName,
+    });
+
+    res.status(201).json({ result: "Success" });
+  } catch (err) {
+    next(err);
   }
-
-  if (!mongoose.isValidObjectId(userId)) {
-    return next(createError(400, ERROR.BAD_REQUEST));
-  }
-
-  const user = await User.findById(userId);
-
-  if (!user) {
-    return next(createError(400, ERROR.BAD_REQUEST));
-  }
-
-  await Project.create({
-    creator: userId,
-    name: projectName,
-  });
-
-  res.status(201).json({ result: "Success" });
 };
--- src/models/Project.js
+++ src/models/Project.js
@@ -6,37 +6,40 @@ const deployStateSchema = new Schema({
   deployedAt: { type: Date },
 });
 
-const projectSchema = new Schema({
-  creator: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
+const projectSchema = new Schema(
+  {
+    creator: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      maxLength: [50, ERROR.MORE_THAN_MAXLENGTH],
+      required: true,
+    },
+    htmlFile: {
+      type: String,
+      default: "",
+    },
+    cssFile: {
+      type: String,
+      default: "",
+    },
+    jsFile: {
+      type: String,
+      default: "",
+    },
+    deployLink: {
+      type: String,
+      default: "",
+    },
+    deployState: {
+      type: deployStateSchema,
+    },
   },
-  name: {
-    type: String,
-    maxLength: [50, ERROR.MORE_THAN_MAXLENGTH],
-    required: true,
-  },
-  htmlFile: {
-    type: String,
-    default: "",
-  },
-  cssFile: {
-    type: String,
-    default: "",
-  },
-  jsFile: {
-    type: String,
-    default: "",
-  },
-  deployLink: {
-    type: String,
-    default: "",
-  },
-  deployState: {
-    type: deployStateSchema,
-  },
-});
+  { timestamps: true },
+);
 
 const Project = model("Project", projectSchema);
 
