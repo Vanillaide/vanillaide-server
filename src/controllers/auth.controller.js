a0f4889466a5d1847173a82cc0f9aedd9f894eea
jisuhan <jessie.jisu.h@gmail.com>
2022-10-20 10:38:27 +0900

    feat: implement postSignUp

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,39 @@
-exports.postSignUp = () => {};
+const createError = require("http-errors");
+const ERROR = require("../constants/error");
+const REGEX = require("../constants/validateCondition");
+const User = require("../models/User");
+
+exports.postSignUp = async (req, res, next) => {
+  try {
+    const { username, email, password, passwordConfirm } = req.body;
+
+    if (
+      !username ||
+      !email ||
+      !password ||
+      !passwordConfirm ||
+      !REGEX.NAME.test(username) ||
+      !REGEX.EMAIL.test(email) ||
+      !REGEX.PASSWORD.test(password) ||
+      !REGEX.PASSWORD.test(passwordConfirm) ||
+      passwordConfirm !== password
+    ) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    const registeredUser = await User.findOne({ email });
+
+    if (registeredUser) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    await User.create({ username, email, password });
+
+    res.status(200).json({ result: "Success" });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.postLogIn = () => {};
 
