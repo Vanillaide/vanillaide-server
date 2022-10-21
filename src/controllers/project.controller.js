e24d7f02cb3119455a673f1e7384b6462d66c415
Ye sohyeon <93248242+yesohyeon@users.noreply.github.com>
2022-10-21 12:16:06 +0900

    feat: implement create, read, delete of projects
    * feat: implement creating a project
    
    * feat: implement getProjects and add time stamp in project schema
    
    * feat: implement deleting a project
    
    * refactor: send a created project information
    
    * refactor: use exists function to find a user

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,6 +1,26 @@
-exports.deleteProject = () => {};
+const mongoose = require("mongoose");
+const createError = require("http-errors");
+
+const Project = require("../models/Project");
+const ERROR = require("../constants/error");
 
 exports.patchProject = () => {};
+
+exports.deleteProject = async (req, res, next) => {
+  try {
+    const { projectId } = req.params;
+
+    if (!projectId || !mongoose.isValidObjectId(projectId)) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    await Project.findByIdAndDelete(projectId);
+
+    res.status(200).json({ result: "Success" });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.postDeployment = () => {};
 
