ab5abc6d79b2faea50058a8d3f63f60891ce1f36
Sohyeon <needtlc13@gmail.com>
2022-10-23 13:24:20 +0900

    feat: implement save code in db

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -4,7 +4,34 @@ const createError = require("http-errors");
 const Project = require("../models/Project");
 const ERROR = require("../constants/error");
 
-exports.patchProject = () => {};
+exports.patchProject = async (req, res, next) => {
+  try {
+    const { projectId } = req.params;
+    const {
+      code: { html, css, js },
+    } = req.body;
+
+    if (!projectId || !mongoose.isValidObjectId(projectId)) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    const project = await Project.findById(projectId);
+
+    if (!project) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    project.htmlFile = html;
+    project.cssFile = css;
+    project.jsFile = js;
+
+    await project.save();
+
+    res.status(200).json({ result: "Success" });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.deleteProject = async (req, res, next) => {
   try {
