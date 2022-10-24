4be0065bf7d69ef3ce61fddeba30fcf601a25105
taeuk <dcjimin@gmail.com>
2022-10-24 11:18:11 +0900

    refactor: remove static setting

--- src/loaders/server.js
+++ src/loaders/server.js
@@ -12,5 +12,4 @@ module.exports = (app) => {
   app.use(cors());
   app.set("views", path.join(__dirname, "../views"));
   app.set("view engine", "ejs");
-  app.use("/static", express.static(path.join(__dirname, "../views")));
 };
