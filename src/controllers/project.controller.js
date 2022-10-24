8504b33034954a2bb00d7181c893923955acb9c5
taeuk <dcjimin@gmail.com>
2022-10-24 11:47:15 +0900

    refactor: add try catch at getDeploment

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -83,17 +83,21 @@ exports.postDeployment = async (req, res, next) => {
 };
 
 exports.getDeployment = async (req, res, next) => {
-  const { projectId } = req.params;
+  try {
+    const { projectId } = req.params;
 
-  if (!projectId || !mongoose.isValidObjectId(projectId)) {
-    return next(createError(400, ERROR.BAD_REQUEST));
+    if (!projectId || !mongoose.isValidObjectId(projectId)) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    const project = await Project.findById(projectId).lean();
+
+    if (!project || !project.deployLink) {
+      return next(createError(400, ERROR.BAD_REQUEST));
+    }
+
+    res.render("template", { project });
+  } catch (err) {
+    next(err);
   }
-
-  const project = await Project.findById(projectId).lean();
-
-  if (!project || !project.deployLink) {
-    return next(createError(400, ERROR.BAD_REQUEST));
-  }
-
-  res.render("template", { project });
 };
